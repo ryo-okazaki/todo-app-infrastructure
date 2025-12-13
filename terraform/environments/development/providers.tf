f891a803139c8f4cdf160c7701faa3723afe901a
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-13 14:48:29 +0900

    fix: tf fmt

--- terraform/environments/development/providers.tf
+++ terraform/environments/development/providers.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 
-    default_tags {
-        tags = {
-            Environment = "development"
-            ManagedBy   = "terraform"
-        }
+  default_tags {
+    tags = {
+      Environment = "development"
+      ManagedBy   = "terraform"
     }
+  }
 }
 
 provider "aws" {
-    alias  = "virginia"
-    region = "us-east-1"
+  alias  = "virginia"
+  region = "us-east-1"
 }
