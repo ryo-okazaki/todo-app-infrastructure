51a724b3819b458ff520008c09fa746b3ec994fe
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-13 16:32:53 +0900

    feat: import modules in develop

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -11,3 +11,19 @@ module "network" {
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.10.0/24", "10.0.20.0/24"]
 }
+
+module "database" {
+  source = "../../modules/database"
+
+  name = "todo-app-dev"
+
+  # networkモジュールのOutputを利用
+  vpc_id             = module.network.vpc_id
+  vpc_cidr           = module.network.vpc_cidr_block
+  private_subnet_ids = module.network.private_subnet_ids
+
+  # DB設定
+  instance_class = "db.t3.micro"   # 開発用なので最小
+  multi_az       = false           # 開発用なのでSingle-AZ
+  db_password    = var.db_password # 変数から渡す
+}
