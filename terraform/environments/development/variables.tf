4590e79651a629b3cba283cbd1abed847a1672fb
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-14 17:41:47 +0900

    add: variables by domain modules

--- terraform/environments/development/variables.tf
+++ terraform/environments/development/variables.tf
@@ -3,3 +3,18 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "domain_name" {
+  description = "domain name for development environment"
+  type        = string
+}
+
+variable "parent_zone_id" {
+  description = "hosted zone ID of the parent domain"
+  type        = string
+}
+
+variable "dns_account_assume_role" {
+  description = "ARN of the role to assume for cross-account access"
+  type        = string
+}
