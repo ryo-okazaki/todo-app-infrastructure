7e137039d59b5de863fe746f8b40f3d97d54571c
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-14 17:41:56 +0900

    add: providers by domain modules

--- terraform/environments/development/providers.tf
+++ terraform/environments/development/providers.tf
@@ -13,3 +13,13 @@ provider "aws" {
   alias  = "virginia"
   region = "us-east-1"
 }
+
+provider "aws" {
+  alias  = "dns_account"
+  region = "ap-northeast-1" # Route53はGlobalだが指定が必要
+
+  assume_role {
+    # Step 1で作ったロールのARN
+    role_arn = var.dns_account_assume_role
+  }
+}
