223079cc501a8d5225ac32866c657d74ee71d1c7
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-29 11:50:17 +0900

    add: some variables to load balancer module

--- terraform/modules/load_balancer/variables.tf
+++ terraform/modules/load_balancer/variables.tf
@@ -1,24 +1,59 @@
+# ------------------------------------------------------------------------------
+# Required Variables
+# ------------------------------------------------------------------------------
 variable "name" {
-  description = "リソース名のプレフィックス"
+  description = "Name prefix for resources"
   type        = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID"
+  description = "VPC ID where ALB will be created"
   type        = string
 }
 
 variable "public_subnet_ids" {
-  description = "ALBを配置するパブリックサブネットのIDリスト"
+  description = "List of public subnet IDs for ALB"
   type        = list(string)
 }
 
 variable "acm_certificate_arn" {
-  description = "HTTPSリスナーに使用するACM証明書のARN"
+  description = "ACM certificate ARN for HTTPS listener"
   type        = string
 }
 
+# ------------------------------------------------------------------------------
+# Optional Variables
+# ------------------------------------------------------------------------------
 variable "access_logs_bucket_id" {
-  description = "アクセスログ保存先のS3バケットID"
+  description = "S3 bucket ID for ALB access logs (optional)"
   type        = string
+  default     = null
+}
+
+variable "allow_http_from_cloudfront" {
+  description = "Allow HTTP (80) from CloudFront (for redirect purposes)"
+  type        = bool
+  default     = false
+}
+
+# ------------------------------------------------------------------------------
+# CloudFront Integration Variables
+# ------------------------------------------------------------------------------
+variable "cloudfront_custom_header_name" {
+  description = "Custom header name from CloudFront for origin verification"
+  type        = string
+  default     = null
+}
+
+variable "cloudfront_custom_header_value" {
+  description = "Custom header value from CloudFront for origin verification"
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "tags" {
+  description = "Additional tags for resources"
+  type        = map(string)
+  default     = {}
 }
