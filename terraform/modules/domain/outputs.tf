03fa5e45301b3e297de990ee29a398c5d436a142
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-29 11:41:36 +0900

    add: domain module outputs

--- terraform/modules/domain/outputs.tf
+++ terraform/modules/domain/outputs.tf
@@ -1,19 +1,49 @@
 output "zone_id" {
-  description = "Route53 Zone ID"
+  description = "Route53 Hosted Zone ID"
   value       = aws_route53_zone.this.zone_id
 }
 
-output "zone_name" {
-  description = "Domain name"
-  value       = aws_route53_zone.this.name
+output "zone_name_servers" {
+  description = "Route53 Hosted Zone Name Servers"
+  value       = aws_route53_zone.this.name_servers
 }
 
 output "alb_certificate_arn" {
-  description = "ACM Certificate ARN for ALB (Tokyo)"
-  value       = aws_acm_certificate.alb.arn
+  description = "ACM Certificate ARN for ALB (regional)"
+  value       = aws_acm_certificate_validation.alb.certificate_arn
 }
 
 output "cloudfront_certificate_arn" {
-  description = "ACM Certificate ARN for CloudFront (Virginia)"
-  value       = aws_acm_certificate.cloudfront.arn
+  description = "ACM Certificate ARN for CloudFront (us-east-1)"
+  value       = aws_acm_certificate_validation.cloudfront.certificate_arn
+}
+
+output "domain_name" {
+  description = "Domain name"
+  value       = var.domain_name
+}
+
+output "route53_zone_id" {
+  description = "Route53 Hosted Zone ID"
+  value       = aws_route53_zone.this.zone_id
+}
+
+output "route53_zone_name" {
+  description = "Route53 Hosted Zone name"
+  value       = aws_route53_zone.this.name
+}
+
+output "acm_alb_certificate_arn" {
+  description = "ACM certificate ARN for ALB (Regional)"
+  value       = aws_acm_certificate_validation.alb.certificate_arn
+}
+
+output "acm_cloudfront_certificate_arn" {
+  description = "ACM certificate ARN for CloudFront (Virginia)"
+  value       = aws_acm_certificate_validation.cloudfront.certificate_arn
+}
+
+output "name_servers" {
+  description = "Name servers for the hosted zone"
+  value       = aws_route53_zone.this.name_servers
 }
