375c7c950144388cb39d20c310aad2de047a1df0
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-29 12:31:22 +0900

    fix: make the static value a variable

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -1,21 +1,23 @@
 module "network" {
   source = "../../modules/network"
 
-  name     = "todo-app-dev"
-  vpc_cidr = "10.0.0.0/16"
+  name     = var.name_prefix
+  vpc_cidr = var.vpc_cidr
 
   # 東京リージョンの 1a, 1c
-  azs = ["ap-northeast-1a", "ap-northeast-1c"]
+  azs = var.availability_zones
 
   # subnet CIDR はAZごとに1つずつ指定
-  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs = ["10.0.10.0/24", "10.0.20.0/24"]
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+
+  api_container_domain_suffix = var.api_container_domain_suffix
 }
 
 module "database" {
   source = "../../modules/database"
 
-  name = "todo-app-dev"
+  name = var.name_prefix
 
   # networkモジュールのOutputを利用
   vpc_id             = module.network.vpc_id
@@ -23,16 +25,11 @@ module "database" {
   private_subnet_ids = module.network.private_subnet_ids
 
   # DB設定
-  instance_class = "db.t3.micro"   # 開発用なので最小
-  multi_az       = false           # 開発用なのでSingle-AZ
-  db_password    = var.db_password # 変数から渡す
-}
-
-module "storage" {
-  source = "../../modules/storage"
-
-  name          = "develop.${data.aws_caller_identity.current.account_id}.todo-app"
-  force_destroy = true # 開発環境なのでterraform destroyで消せるようにする
+  instance_class = "db.t3.micro" # 開発用なので最小
+  multi_az       = false         # 開発用なのでSingle-AZ
+  db_name        = var.db_name
+  db_password    = var.db_password
+  db_username    = var.db_username
 }
 
 module "domain" {
@@ -48,10 +45,19 @@ module "domain" {
   }
 }
 
+module "storage" {
+  source = "../../modules/storage"
+
+  env              = var.environment
+  name             = var.name_prefix
+  force_destroy    = true # 開発環境なのでterraform destroyで消せるようにする
+  ecr_repositories = var.ecr_repositories
+}
+
 module "load_balancer" {
   source = "../../modules/load_balancer"
 
-  name = "todo-app-dev"
+  name = var.name_prefix
 
   vpc_id            = module.network.vpc_id
   public_subnet_ids = module.network.public_subnet_ids
@@ -60,5 +66,5 @@ module "load_balancer" {
   acm_certificate_arn = module.domain.alb_certificate_arn
 
   # storageモジュールで作ったログ用バケット
-  access_logs_bucket_id = module.storage.s3_logs_bucket_id
+  access_logs_bucket_id = module.storage.logs_bucket.id
 }
